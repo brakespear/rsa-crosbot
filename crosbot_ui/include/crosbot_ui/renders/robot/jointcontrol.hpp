6353e6b10068f7e0f59f64f6488ce1c09a564435
Rescue Group <rescue@rescuelap2.(none)>
2013-09-26 12:53:02 +1000

    MM: Competition GUI changes.

---
+++ crosbot_ui/include/crosbot_ui/renders/robot/jointcontrol.hpp
@@ -0,0 +1,41 @@
+/*
+ * jointcontrol.hpp
+ *
+ *  Created on: 27/06/2013
+ *      Author: rescue
+ */
+
+#ifndef CROSBOT_JOINTCONTROL_HPP_
+#define CROSBOT_JOINTCONTROL_HPP_
+
+#include <ros/ros.h>
+#include <crosbot/thread.hpp>
+
+namespace crosbot {
+
+class JointController {
+protected:
+	class Joint {
+	public:
+		std::string name;
+		double pos, desiredPos;
+		Joint(const std::string& name) : name(name), pos(NAN), desiredPos(NAN) {}
+	};
+
+
+	static ReadWriteLock jLock;
+	static std::vector< Joint > joints;
+	static Joint *findJoint(const std::string& joint);
+public:
+	static void connect();
+	static void shutdown();
+	static double getPos(const std::string& joint);
+	static void setPos(const std::string& joint, double pos);
+	static void zero(const std::vector< std::string >& joints);
+
+	friend class JointControlConnection;
+};
+
+} // namespace crosbot
+
+#endif /* CROSBOT_JOINTCONTROL_HPP_ */
