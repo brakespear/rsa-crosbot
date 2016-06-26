0c7a48632e564a25cab89f587d00ee9a6fb0e1b4
Timothy Wiley <timothyw@cse.unsw.edu.au>
2016-06-26 16:26:06 +1000

    crosbot: Re-enable use of ros logging in place of printf

--- crosbot/include/crosbot/utils.hpp
+++ crosbot/include/crosbot/utils.hpp
@@ -1,5 +1,5 @@
 /*
- * data.h
+ * utils.h
  *
  *  Created on: 08/08/2011
  *      Author: rescue
@@ -8,25 +8,29 @@
 #ifndef CROSBOT_UTILS_H_
 #define CROSBOT_UTILS_H_
 
-//#ifdef ROS_VERSION
-//
+#ifdef ROS_VERSION
+
 //#ifndef MODULE_NAME
 //#define MODULE_NAME		"crosbot"
 //#endif
-//
-//#define LOG(...)		ROS_LOG(ros::console::levels::Info, MODULE_NAME, __VA_ARGS__)
-//#define DEBUG(...)	ROS_LOG(ros::console::levels::Debug, MODULE_NAME, __VA_ARGS__)
-//#define WARN(...)		ROS_LOG(ros::console::levels::Warn, MODULE_NAME, __VA_ARGS__)
-//#define ERROR(...)	ROS_LOG(ros::console::levels::Error, MODULE_NAME, __VA_ARGS__)
-//
-//#else
+
+//#define LOG(...)        ROS_LOG(ros::console::levels::Info, MODULE_NAME, __VA_ARGS__)
+//#define DEBUG(...)      ROS_LOG(ros::console::levels::Debug, MODULE_NAME, __VA_ARGS__)
+//#define WARN(...)       ROS_LOG(ros::console::levels::Warn, MODULE_NAME, __VA_ARGS__)
+//#define ERROR(...)      ROS_LOG(ros::console::levels::Error, MODULE_NAME, __VA_ARGS__)
+#define LOG(...)        ROS_INFO(__VA_ARGS__)
+#define DEBUG(...)      ROS_DEBUG(__VA_ARGS__)
+#define WARN(...)       ROS_WARN(__VA_ARGS__)
+#define ERROR(...)      ROS_ERROR(__VA_ARGS__)
+
+#else
 
 #define LOG(...)	printf(__VA_ARGS__)
 #define DEBUG(...)	printf(__VA_ARGS__)
 #define WARN(...)	printf(__VA_ARGS__)
 #define ERROR(...)	fprintf(stderr, __VA_ARGS__)
 
-//#endif
+#endif
 
 #ifdef TOPIC_TOOLS_SHAPE_SHIFTER_H
 
